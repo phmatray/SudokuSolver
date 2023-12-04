8f81c5fa526d1dbf6aecfb012a072be47544c54a
Philippe Matray <phmatray@gmail.com>
2023-12-04 09:47:14 +0100

    feature: upgrade to .NET8

--- SudokuSolver.CLI/Program.cs
+++ SudokuSolver.CLI/Program.cs
@@ -1,11 +1,15 @@
-﻿using SudokuSolver.CLI;
+﻿using Microsoft.Extensions.Logging;
+using SudokuSolver.CLI;
 
 Console.WriteLine("Sudoku Solver");
 Console.WriteLine("=============");
 
+var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+var logger = loggerFactory.CreateLogger<Program>();
+
 DependencyInjections
-    .ConfigureAndBuildServices()
-    .RetrieveGameService()
+    .ConfigureAndBuildServices(logger)
+    .RetrieveGameService(logger)
     .RunGame();
 
 Console.ReadLine();
